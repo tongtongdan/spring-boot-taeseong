645cba8738b8bfaf77e1d5e8a91340761e461a22
busicoder <judge060217@gmail.com>
2026-07-15 11:37:46 +0900

    init

---
+++ src/main/java/likelion14th/springstudy/TongtongdanApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongtongdanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongtongdanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/likelion14th/springstudy/TongtongdanApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion14th.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongtongdanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
