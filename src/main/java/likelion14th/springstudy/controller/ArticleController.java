17a2510e907cd6a37f2099405bc169f6a19100d0
busicoder <judge060217@gmail.com>
2026-08-11 18:20:16 +0900

    feat: 단일 게시글 조회하기

--- src/main/java/likelion14th/springstudy/controller/ArticleController.java
+++ src/main/java/likelion14th/springstudy/controller/ArticleController.java
@@ -7,10 +7,7 @@ import likelion14th.springstudy.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +18,12 @@ public class ArticleController {
     public ResponseEntity<ApiResponse<ArticleResponse>> addArticle(@RequestBody ArticleRequest request){
         ArticleResponse articleResponse = articleService.addArticle(request.getTitle(),request.getContent(),request.getAuthor(),request.getPassword());
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(201,"게시글 생성에 성공하였습니다.",articleResponse));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<ArticleResponse>> getArticle(@PathVariable Long id){
+
+        ArticleResponse articleResponse = articleService.getArticle(id);
+        return ResponseEntity.ok(ApiResponse.success(201,"게시글 조회에 성공하였습니다.",articleResponse));
     }
 }
--- src/main/java/likelion14th/springstudy/service/ArticleService.java
+++ src/main/java/likelion14th/springstudy/service/ArticleService.java
@@ -1,5 +1,6 @@
 package likelion14th.springstudy.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import likelion14th.springstudy.domain.Article;
 import likelion14th.springstudy.dto.response.ArticleResponse;
@@ -16,6 +17,11 @@ public class ArticleService {
     public ArticleResponse addArticle(String title, String content, String author,String password){
         Article article = new Article(title,content,author,password);
         articleRepository.save(article);//JPA를 사용해서 저장
+        return ArticleResponse.from(article);
+    }
+    @Transactional
+    public ArticleResponse getArticle(Long Id){
+        Article article = articleRepository.findById(Id).orElseThrow(()-> new EntityNotFoundException("해당 Id의 게시글이 없습니다."));
         return ArticleResponse.from(article);
     }
 }
